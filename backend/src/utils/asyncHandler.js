0ad35fee7333bbb7b51ba07d953fa5046a8f9261
HJ-ANAND <anandharshjeet054@gmail.com>
2026-04-11 11:44:41 +0530

    added utils

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+// Try catch
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 400).json({
+//             success: false,
+//             message: err.message
+//         })
+
+//     }
+// }
